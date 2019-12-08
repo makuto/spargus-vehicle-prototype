72129b7d275ca98b186c7d5a0a9bd603dfc24c48
Macoy Madson <macoymadson@gmail.com>
2019-12-08 15:31:26 -0800

    Add buttons for changing engine power

--- src/Joystick.cpp
+++ src/Joystick.cpp
@@ -16,35 +16,52 @@
 
 const float joystickRange = 100.f;
 
+void printJoystickButtonPresses()
+{
+	int playerJoystickId = 0;
+	bool connected = sf::Joystick::isConnected(playerJoystickId);
+	if (!connected)
+		return;
+	// How many buttons does joystick #0 support?
+	unsigned int buttons = sf::Joystick::getButtonCount(playerJoystickId);
+	for (int i = 0; i < buttons; ++i)
+	{
+		bool buttonPressed = sf::Joystick::isButtonPressed(playerJoystickId, i);
+		if (buttonPressed)
+			std::cout << "Pressed button " << i << "\n";
+	}
+}
+
 void printJoystickInput()
 {
+	int playerJoystickId = 0;
+
 	// Is joystick #0 connected?
-	bool connected = sf::Joystick::isConnected(0);
+	bool connected = sf::Joystick::isConnected(playerJoystickId);
 	// How many buttons does joystick #0 support?
-	unsigned int buttons = sf::Joystick::getButtonCount(0);
+	unsigned int buttons = sf::Joystick::getButtonCount(playerJoystickId);
 	// Does joystick #0 define a X axis?
-	bool hasX = sf::Joystick::hasAxis(0, sf::Joystick::X);
-	// Is button #2 pressed on joystick #0? (XB)
-	bool pressed = sf::Joystick::isButtonPressed(0, 2);
+	bool hasX = sf::Joystick::hasAxis(playerJoystickId, sf::Joystick::X);
 
 	// What's the current position of the X axis on joystick #0? (Left thumbstick; -100 = left)
-	float positionX = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
+	float positionX = sf::Joystick::getAxisPosition(playerJoystickId, sf::Joystick::X);
 	// What's the current position of the Y axis on joystick #0? (Left thumbstick; -100 = forward)
-	float positionY = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
+	float positionY = sf::Joystick::getAxisPosition(playerJoystickId, sf::Joystick::Y);
 	// Left trigger pressed = 100 (unpressed = -100)
-	float positionZ = sf::Joystick::getAxisPosition(0, sf::Joystick::Z);
+	float positionZ = sf::Joystick::getAxisPosition(playerJoystickId, sf::Joystick::Z);
 	// Right trigger pressed = 100 (unpressed = -100)
-	float positionR = sf::Joystick::getAxisPosition(0, sf::Joystick::R);
+	float positionR = sf::Joystick::getAxisPosition(playerJoystickId, sf::Joystick::R);
 	// Right joystick X left -100
-	float positionU = sf::Joystick::getAxisPosition(0, sf::Joystick::U);
+	float positionU = sf::Joystick::getAxisPosition(playerJoystickId, sf::Joystick::U);
 	// Right joystick Y forward -100
-	float positionV = sf::Joystick::getAxisPosition(0, sf::Joystick::V);
+	float positionV = sf::Joystick::getAxisPosition(playerJoystickId, sf::Joystick::V);
 
 	std::cout << "connected = " << connected << " buttons = " << buttons << " hasX = " << hasX
-	          << " pressed = " << pressed << " \npositionX = " << positionX
-	          << " positionY = " << positionY << " positionZ = " << positionZ
-	          << " positionR = " << positionR << " positionU = " << positionU
-	          << " positionV = " << positionV << "\n";
+	          << " \npositionX = " << positionX << " positionY = " << positionY
+	          << " positionZ = " << positionZ << " positionR = " << positionR
+	          << " positionU = " << positionU << " positionV = " << positionV << "\n";
+
+	printJoystickButtonPresses();
 }
 
 void applyDeadzone(float& joystickValue)
@@ -63,7 +80,52 @@ float interpolateRange(float startA, float endA, float startB, float endB, float
 void processVehicleInputJoystick(PhysicsVehicle& vehicle)
 {
 	bool debugPrint = false;
-	
+
+	// printJoystickButtonPresses();
+
+	// Is joystick #0 connected?
+	int playerJoystickId = 0;
+	bool controllerConnected = sf::Joystick::isConnected(playerJoystickId);
+	// TODO Support controller select (later in project)
+	if (!controllerConnected)
+		return;
+
+	// Start button resets vehicle
+	bool startPressed = sf::Joystick::isButtonPressed(playerJoystickId, 7);
+	if (startPressed)
+		vehicle.Reset();
+
+	// Change tuning via shoulder buttons
+	if (sf::Joystick::isButtonPressed(playerJoystickId, 5))
+	{
+		vehicle.maxEngineForce += 100.f;
+		std::cout << "Set maxEngineForce to " << vehicle.maxEngineForce << "\n";
+	}
+	else if (sf::Joystick::isButtonPressed(playerJoystickId, 4))
+	{
+		vehicle.maxEngineForce -= 100.f;
+		std::cout << "Set maxEngineForce to " << vehicle.maxEngineForce << "\n";
+	}
+
+	if (vehicle.maxEngineForce < 0.f)
+		vehicle.maxEngineForce = 0.f;
+
+	// -100 = left thumbstick up, 100 = down
+	// float position = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
+
+	// -100 = left thumbstick up, 100 = down
+	float turningPosition = sf::Joystick::getAxisPosition(playerJoystickId, sf::Joystick::X) * -1.f;
+
+	// Right trigger pressed = 100 (unpressed = -100)
+	float throttlePosition = sf::Joystick::getAxisPosition(playerJoystickId, sf::Joystick::R);
+
+	// Left trigger pressed = 100 (unpressed = -100)
+	float brakePosition = sf::Joystick::getAxisPosition(playerJoystickId, sf::Joystick::Z);
+
+	applyDeadzone(turningPosition);
+	applyDeadzone(throttlePosition);
+	applyDeadzone(brakePosition);
+
 	bool useGameSteering = true;
 	// Reset steering and forces immediately
 	if (useGameSteering)
@@ -73,37 +135,12 @@ void processVehicleInputJoystick(PhysicsVehicle& vehicle)
 		vehicle.VehicleSteering = 0.f;
 	}
 
-	// Is joystick #0 connected?
-	bool connected = sf::Joystick::isConnected(0);
-	// TODO Support controller select (later in project)
-	if (!connected)
-		return;
-
-	// X button
-	// bool pressed = sf::Joystick::isButtonPressed(0, 2);
-	
-	// -100 = left thumbstick up, 100 = down
-	// float position = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
-	
-	// -100 = left thumbstick up, 100 = down
-	float turningPosition = sf::Joystick::getAxisPosition(0, sf::Joystick::X) * -1.f;
-	
-	// Right trigger pressed = 100 (unpressed = -100)
-	float throttlePosition = sf::Joystick::getAxisPosition(0, sf::Joystick::R);
-	
-	// Left trigger pressed = 100 (unpressed = -100)
-	float brakePosition = sf::Joystick::getAxisPosition(0, sf::Joystick::Z);
-
-	applyDeadzone(turningPosition);
-	applyDeadzone(throttlePosition);
-	applyDeadzone(brakePosition);
-
 	vehicle.VehicleSteering = interpolateRange(-vehicle.steeringClamp, vehicle.steeringClamp,
 	                                           -joystickRange, joystickRange, turningPosition);
-	vehicle.EngineForce = interpolateRange(0.f, vehicle.maxEngineForce,
-	                                       -joystickRange, joystickRange, throttlePosition);
-	vehicle.BrakingForce = interpolateRange(0.f, vehicle.maxBrakingForce,
-	                                        -joystickRange, joystickRange, brakePosition);
+	vehicle.EngineForce = interpolateRange(0.f, vehicle.maxEngineForce, -joystickRange,
+	                                       joystickRange, throttlePosition);
+	vehicle.BrakingForce = interpolateRange(0.f, vehicle.maxBrakingForce, -joystickRange,
+	                                        joystickRange, brakePosition);
 
 	btScalar speedKmHour = vehicle.vehicle->getCurrentSpeedKmHour();
 
