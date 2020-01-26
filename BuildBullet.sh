6aa560a83b1ec0782ead6595c6ca81b784488530
Macoy Madson <macoymadson@gmail.com>
2020-01-26 13:01:35 -0800

    Fix release build requiring pybullet too

--- BuildBullet.sh
+++ BuildBullet.sh
@@ -7,8 +7,9 @@ fi
 mkdir -p build_cmake
 cd build_cmake
 # cmake -DBUILD_PYBULLET=ON -DBUILD_PYBULLET_NUMPY=ON -DUSE_DOUBLE_PRECISION=ON -DBT_USE_EGL=ON -DCMAKE_BUILD_TYPE=Debug .. || exit 1
-# Macoy was here: Disable PyBullet and use single precision instead
-cmake -DBT_USE_EGL=ON -DCMAKE_BUILD_TYPE=Release .. || exit 1
+# Macoy was here: Use single precision instead
+# Pybullet is necessary for world serialization stuff (why?)
+cmake -DBUILD_PYBULLET=ON -DBUILD_PYBULLET_NUMPY=ON -DBT_USE_EGL=ON -DCMAKE_BUILD_TYPE=Release .. || exit 1
 make -j $(command nproc 2>/dev/null || echo 12) || exit 1
 
 echo "Completed build of Bullet."
