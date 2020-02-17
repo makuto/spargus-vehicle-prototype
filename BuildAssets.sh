9d48b85557bcff2b315dd7271e444bc83aa9abee
Macoy Madson <macoymadson@gmail.com>
2020-02-17 12:00:34 -0800

    Updated readme, disabled BuildAssets.sh

--- BuildAssets.sh
+++ BuildAssets.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
 
-echo "Exporting all .blend files in assets..."
-echo "(create Horde Collada and Bullet Obj)"
-cd assets
-for blendFile in *.blend
-do
-	echo "---------------------------------------------"
-	echo "$blendFile"
-	blender $blendFile --background --python ExportBlenderAssets.py
-done
-echo "Done\n"
+echo "This script is now deprecated!"
+echo "Use jam instead"
 
-echo "============================================="
-echo "Copying obj output to Collision..."
-rsync -a --stats *.obj ../Collision/
-echo "Done\n"
+# echo "Exporting all .blend files in assets..."
+# echo "(create Horde Collada and Bullet Obj)"
+# cd assets
+# for blendFile in *.blend
+# do
+# 	echo "---------------------------------------------"
+# 	echo "$blendFile"
+# 	blender $blendFile --background --python ExportBlenderAssets.py
+# done
+# echo "Done\n"
 
-echo "============================================="
-echo "Copying textures to Content..."
-rsync -a --stats *.png ../Content/textures/
-rsync -a --stats *.jpg ../Content/textures/
-echo "Done\n"
+# echo "============================================="
+# echo "Copying obj output to Collision..."
+# rsync -a --stats *.obj ../Collision/
+# echo "Done\n"
 
-cd ..
+# echo "============================================="
+# echo "Copying textures to Content..."
+# rsync -a --stats *.png ../Content/textures/
+# rsync -a --stats *.jpg ../Content/textures/
+# echo "Done\n"
 
-echo "============================================="
-echo "Copying Collada output to Horde Content format..."
-./Horde3D/build/Binaries/Linux/Debug/ColladaConv assets -type model -addModelName -dest Content
-echo "Done"
+# cd ..
+
+# echo "============================================="
+# echo "Copying Collada output to Horde Content format..."
+# ./Horde3D/build/Binaries/Linux/Debug/ColladaConv assets -type model -addModelName -dest Content
+# echo "Done"
